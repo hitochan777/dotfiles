542418db5259d25201393e8de483709b7e49a622
Hitoshi Otsuki <hitochan777@gmail.com>
2016-11-13 17:00:12 +0900

    Add dein.toml and install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+mkdir -p $HOME/.config/nvim
+
+ln -s $PWD/vimrc $HOME/.vimrc
+ln -s $PWD/vimrc $HOME/.config/nvim/init.vim
+ln -s $PWD/dein.toml $HOME/.config/nvim/dein.toml
+
+ln -s $PWD/zshrc $HOME/.zshrc
+ln -s $PWD/tmux.conf $HOME/.tmux.conf
