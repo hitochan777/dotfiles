3ab5639de097d6fbe0dbef854ccdf8faf1f79c1e
Otsuki Hitoshi <hitochan777@gmail.com>
2018-01-27 22:21:32 +0900

    chore: Add czrc

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ ln -s $PWD/vimrc $HOME/.config/nvim/init.vim
 ln -s $PWD/dein.toml $HOME/.config/nvim/dein.toml
 ln -s $PWD/ideavimrc $HOME/.ideavimrc
 ln -s $PWD/latexmkrc $HOME/.latexmkrc
-
 ln -s $PWD/zshrc $HOME/.zshrc
 ln -s $PWD/tmux.conf $HOME/.tmux.conf
+ln -s $PWD/czrc $HOME/.czrc
