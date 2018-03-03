1831fe6303758056e8719b7d2da964478cb707cf
Otsuki Hitoshi <hitochan777@gmail.com>
2018-03-03 04:34:32 +0000

    Enable running command anywhere

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
 mkdir -p $HOME/.config/nvim
 
-ln -s $PWD/vimrc $HOME/.vimrc
-ln -s $PWD/vimrc $HOME/.config/nvim/init.vim
-ln -s $PWD/dein.toml $HOME/.config/nvim/dein.toml
-ln -s $PWD/ideavimrc $HOME/.ideavimrc
-ln -s $PWD/latexmkrc $HOME/.latexmkrc
-ln -s $PWD/zshrc $HOME/.zshrc
-ln -s $PWD/tmux.conf $HOME/.tmux.conf
-ln -s $PWD/czrc $HOME/.czrc
+ln -s $SCRIPTPATH/vimrc $HOME/.vimrc
+ln -s $SCRIPTPATH/vimrc $HOME/.config/nvim/init.vim
+ln -s $SCRIPTPATH/dein.toml $HOME/.config/nvim/dein.toml # for nvim
+ln -s $SCRIPTPATH/dein.toml $HOME/dein.toml # for vim
+ln -s $SCRIPTPATH/ideavimrc $HOME/.ideavimrc
+ln -s $SCRIPTPATH/latexmkrc $HOME/.latexmkrc
+ln -s $SCRIPTPATH/zshrc $HOME/.zshrc
+ln -s $SCRIPTPATH/tmux.conf $HOME/.tmux.conf
+ln -s $SCRIPTPATH/czrc $HOME/.czrc
