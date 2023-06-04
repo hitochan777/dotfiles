78f3b00826fe196f381ce0509b2e936d8f10c539
Otsuki Hitoshi <hitochan777@gmail.com>
2023-06-05 00:11:50 +0900

    Fix install script

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-mkdir -p $HOME/.config/nvim
-
-ln -s $SCRIPTPATH/init.lua $HOME/.config/nvim/init.lua
 ln -s $SCRIPTPATH/ideavimrc $HOME/.ideavimrc
 ln -s $SCRIPTPATH/latexmkrc $HOME/.latexmkrc
 ln -s $SCRIPTPATH/zshrc $HOME/.zshrc
@@ -10,6 +7,8 @@ ln -s $SCRIPTPATH/zprofile $HOME/.zprofile
 ln -s $SCRIPTPATH/tmux.conf $HOME/.tmux.conf
 ln -s $SCRIPTPATH/czrc $HOME/.czrc
 ln -s $SCRIPTPATH/gitconfig $HOME/.gitconfig
-ln -s $SCRIPTPATH/nvim $HOME/.config/
-ln -s $SCRIPTPATH/nvchad/custom $HOME/.config/nvim/lua/
 
+# nvim
+ln -s $SCRIPTPATH/nvim $HOME/.config/
+rm -rf ~/.config/nvim/lua/custom
+ln -s $SCRIPTPATH/nvchad/custom $HOME/.config/nvim/lua/
