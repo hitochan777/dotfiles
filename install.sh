a904c534a885c2a20c93953d7aced1dce24fc71a
Otsuki Hitoshi <hitochan777@gmail.com>
2026-08-02 17:39:42 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-# ln -s $SCRIPTPATH/ideavimrc $HOME/.ideavimrc
-# ln -s $SCRIPTPATH/latexmkrc $HOME/.latexmkrc
-# ln -s $SCRIPTPATH/zshrc $HOME/.zshrc
-# ln -s $SCRIPTPATH/zprofile $HOME/.zprofile
-# ln -s $SCRIPTPATH/tmux.conf $HOME/.tmux.conf
-# ln -s $SCRIPTPATH/czrc $HOME/.czrc
-# ln -s $SCRIPTPATH/gitconfig $HOME/.gitconfig
+ln -s $SCRIPTPATH/ideavimrc $HOME/.ideavimrc
+ln -s $SCRIPTPATH/latexmkrc $HOME/.latexmkrc
+ln -s $SCRIPTPATH/zshrc $HOME/.zshrc
+ln -s $SCRIPTPATH/zprofile $HOME/.zprofile
+ln -s $SCRIPTPATH/tmux.conf $HOME/.tmux.conf
+ln -s $SCRIPTPATH/gitconfig $HOME/.gitconfig
 
 # nvim
 ln -s $SCRIPTPATH/nvim $HOME/.config/
+
+# Claude Code
+bash $SCRIPTPATH/install-claude.sh
