40dfaa721f66fd0fd9798541e00457f0b59a3046
Otsuki Hitoshi <hitochan777@gmail.com>
2026-08-02 17:37:21 +0900

    Add LazyVim to dotfiles

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
